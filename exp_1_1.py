7478fb06c360edc1ba35fc5a96114df082058ce4
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-09-23 16:26:10 +0530

    finishing the experiment 1

--- exp_1_1.py
+++ exp_1_1.py
@@ -31,28 +31,47 @@
 # W.A.P to understand bitwise operator
 
 # Take two integer inputs from the user
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
 
 # Bitwise AND
-print(f"Bitwise AND of {num1} and {num2} is {num1 & num2}")
+# print(f"Bitwise AND of {num1} and {num2} is {num1 & num2}")
 
 # Bitwise OR
-print(f"Bitwise OR of {num1} and {num2} is {num1 | num2}")
+# print(f"Bitwise OR of {num1} and {num2} is {num1 | num2}")
 
 # Bitwise XOR
-print(f"Bitwise XOR of {num1} and {num2} is {num1 ^ num2}")
+# print(f"Bitwise XOR of {num1} and {num2} is {num1 ^ num2}")
 
 # Bitwise NOT
-print(f"Bitwise NOT of {num1} is {~num1}")
-print(f"Bitwise NOT of {num2} is {~num2}")
+# print(f"Bitwise NOT of {num1} is {~num1}")
+# print(f"Bitwise NOT of {num2} is {~num2}")
 
 # Left Shift
-shift = int(input("Enter number of positions to shift left: "))
-print(f"{num1} left shifted by {shift} positions is {num1 << shift}")
-print(f"{num2} left shifted by {shift} positions is {num2 << shift}")
+# shift = int(input("Enter number of positions to shift left: "))
+# print(f"{num1} left shifted by {shift} positions is {num1 << shift}")
+# print(f"{num2} left shifted by {shift} positions is {num2 << shift}")
 
 # Right Shift
-shift = int(input("Enter number of positions to shift right: "))
-print(f"{num1} right shifted by {shift} positions is {num1 >> shift}")
-print(f"{num2} right shifted by {shift} positions is {num2 >> shift}")
+# shift = int(input("Enter number of positions to shift right: "))
+# print(f"{num1} right shifted by {shift} positions is {num1 >> shift}")
+# print(f"{num2} right shifted by {shift} positions is {num2 >> shift}")
+
+# W.A.P to understand assignment operator
+count = int(input("Enter a number: "))
+count += 1
+print(f"count = {count}")
+count -= 1
+print(f"count = {count}")
+count *= 2
+print(f"count = {count}")
+count /= 2
+print(f"count = {count}")
+count //= 2
+print(f"count = {count}")
+count **= 2
+print(f"count = {count}")
+count %= 3
+print(f"count = {count}")
+
+#W.A.P that takes 3 numbers as input from the user and perform following operations on three numbers multiplication, substraction and addition
