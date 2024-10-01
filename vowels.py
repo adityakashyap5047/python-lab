3bb553a5321f59b3d3153a0359bec846020eefa2
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-10-01 15:53:46 +0530

    enhancing the code for vowels

--- vowels.py
+++ vowels.py
@@ -5,7 +5,7 @@ my_str = input("Enter a string: ")
 count = 0
 
 for i in my_str:
-    if i in "aeiouAEIOU":
+    if i.lower() in "aeiou":
         count += 1
 
 print(f"Number of vowels in the string: {count}")
