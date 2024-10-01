f241146fd465f6f6e32fc939142a96e050020872
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-10-01 16:22:50 +0530

    Add set operations to perform union, intersection, and difference

---
+++ set.py
@@ -0,0 +1,13 @@
+#Write a python program to create 2 set of integers and perform the following operations
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+#1. Union
+print("Union of two sets: ", set1.union(set2))
+
+#2. Intersection
+print("Intersection of two sets: ", set1.intersection(set2))
+
+#3. Difference
+print("Difference of two sets: ", set1.difference(set2))
