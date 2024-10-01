4d1bf17fbbc6b453e8f8e610a631fd78145152f9
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-10-01 16:09:05 +0530

    perform operation on tuple

--- temp.py
+++ temp.py
@@ -1,19 +1,10 @@
-# Write a python program to perform following operations on tuple
-
-tup_1 = (1, 2, 3, 4, 5)
-tup_2 = (6, 7, 8, 9, 10)
-
-# concatenation of tuple
-tup_3 = tup_1 + tup_2
-print(tup_3)
-
-# repetition of tuple
-tup_4 = tup_1 * 2
-print(tup_4)
-
-# indexing
-print(tup_1[0])
-print(tup_1[2])
-
-# slicing
-print(tup_2[1:3])
+# Write a python program to take 5 numbers as input from user and store them in a tuple and then calculate the sum of number
+li = []
+for i in range(5):
+    num = int(input(f"Enter {i+1}th number: "))
+    li.append(num)
+tup = tuple(li)
+sum = 0
+for i in tup:
+    sum = sum + i
+print(sum)
---
+++ tup.py
@@ -0,0 +1,19 @@
+# Write a python program to perform following operations on tuple
+
+tup_1 = (1, 2, 3, 4, 5)
+tup_2 = (6, 7, 8, 9, 10)
+
+# concatenation of tuple
+tup_3 = tup_1 + tup_2
+print(tup_3)
+
+# repetition of tuple
+tup_4 = tup_1 * 2
+print(tup_4)
+
+# indexing
+print(tup_1[0])
+print(tup_1[2])
+
+# slicing
+print(tup_2[1:3])
